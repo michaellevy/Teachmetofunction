d4859d4097621544e300ecb1d10fc7879723d112
Myfanwy <mrowlan1@gmail.com>
2015-01-27 20:35:05 -0800

    NameThatKid.v1.0

---
+++ NameThatKid.r
@@ -0,0 +1,33 @@
+
+## Title: "Name That Kid r function"
+## Date: 27-Jan-2015
+## File: NameThatKid.r
+## Author(s): Myfanwy Johnston, Noam Ross
+## Purpose: To annoy all your pregnant friends with a selection of five random names from the decade of your choice.
+
+## Current limitations: can only select male or female (not a random selection of both genders), clunky code structure.
+ # Weighs all names equally, not by popularity.  No options for twins :)  Have to put gender in quotes (is that normal?)
+
+## Depends upon: dplyr, plyr, babynames
+
+require(dplyr)
+require(babynames)
+
+NameThatKid <- function(gender, decade) {
+  a <- babynames %>%
+    filter(sex==gender) %>%
+    # filter(sex=='M') %>% # for function testing
+    mutate(dec = plyr::round_any(year, accuracy=10, f=floor)) %>% # Thanks Noam!
+    group_by(dec) %>%
+    sample_n(., 5)
+  
+  b <- a %>%
+    filter(dec == decade)
+  # filter(dec==1990) # for function testing
+  c <- ungroup(b)
+  c <- select(c, year, name, n)
+  return(c)
+}
+
+#Example of use:
+NameThatKid(gender= 'M', decade=2000)
